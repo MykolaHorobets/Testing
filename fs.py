4be66813e70cbfce7471a260c31cf292f909977e
mykola <mykola2002@gmail.com>
2023-03-30 00:06:13 +0300

    flask app

--- fs.py
+++ fs.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Directory:
     def __init__(self, dir_name, max_elements=0, father=None):
         self.father = father
@@ -39,13 +42,13 @@ class Directory:
 
 class BinaryFile:
     def __init__(self, id, file_name, info=None, father=None):
-        self.fileName = file_name
+        self.file_name = file_name
         self.info = info
         self.father = father
         self.id = id
 
     def __delete__(self):
-        print('Destructor called, ' + self.fileName + 'was deleted')
+        print('Destructor called, ' + self.file_name + 'was deleted')
         return
 
     def move(self, path):
@@ -63,12 +66,12 @@ class BinaryFile:
 
 class LogFile:
     def __init__(self, file_name, info, father=None):
-        self.fileName = file_name
+        self.file_name = file_name
         self.father = father
         self.info = info
 
     def __delete__(self):
-        print('Destructor called', + self.fileName + 'was deleted')
+        print('Destructor called', + self.file_name + 'was deleted')
         return
 
     def move(self, path):
@@ -90,7 +93,7 @@ class LogFile:
 
 class BufferFile:
     def __init__(self, file_name, max_size=0, father=None):
-        self.fileName = file_name
+        self.file_name = file_name
         self.father = father
         self.info = []
         self.MAX_BUF_FILE_SIZE = max_size
@@ -120,3 +123,184 @@ class BufferFile:
             self.info.pop(0)
             return temp
         return None
+
+
+class FileSystem:
+    def __init__(self, root):
+        self.root = Directory("root")
+
+    def find_item(self, item_name, current_dir=None):
+        if current_dir is None:
+            current_dir = self.root
+        for item in current_dir.listOfFiles:
+            if item.file_name == item_name:
+                return item
+            if isinstance(item, Directory):
+                found_item = self.find_item(item_name, item)
+                if found_item:
+                    return found_item
+        return None
+
+    def find_directory(self, path):
+        if path == "root":
+            return self.root
+
+        path_parts = path.split('/')
+        current_dir = self.root
+        for part in path_parts:
+            if part == '':
+                continue
+            found = False
+            for item in current_dir.listOfFiles:
+                if isinstance(item, Directory) and item.dirName == part:
+                    current_dir = item
+                    found = True
+                    break
+            if not found:
+                print(f"Path '{path}' not found.")
+                return None
+        return current_dir
+
+    def get_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            if isinstance(item, (BinaryFile, LogFile, BufferFile)):
+                item_data = {
+                    'name': item.file_name,
+                    'info': item.read()
+                }
+            elif isinstance(item, Directory):
+                item_data = {
+                    'name': item.dirName,
+                    'contents': json.loads(self.get_items(item))
+                }
+            else:
+                print(f"Item '{item_name}' not found.")
+                return None
+            return json.dumps(item_data, indent=2)
+        else:
+            print(f"Item '{item_name}' not found.")
+            return None
+
+    def remove_item(self, item_name, current_dir=None):
+        if current_dir is None:
+            current_dir = self.root
+        for i, item in enumerate(current_dir.listOfFiles):
+            if item.file_name == item_name:
+                current_dir.listOfFiles.pop(i)
+                current_dir.numberOfElements -= 1
+                item.father = None
+                return
+            if isinstance(item, Directory):
+                self.remove_item(item_name, item)
+        print(f"Item '{item_name}' not found.")
+
+    def move_item(self, current_path, destination_path):
+        item = self.get_item_from_directory(current_path)
+        if item:
+            target_dir = self.find_directory(destination_path)
+            if target_dir:
+                if target_dir.numberOfElements < target_dir.DIR_MAX_ELEMS or target_dir.DIR_MAX_ELEMS == 0:
+                    item.father.listOfFiles.remove(item)
+                    item.father.numberOfElements -= 1
+                    target_dir.listOfFiles.append(item)
+                    target_dir.numberOfElements += 1
+                    item.father = target_dir
+                    print(f"Item '{item.file_name}' moved from '{current_path}' to '{destination_path}'.")
+                else:
+                    print(f"Destination directory '{target_dir.dirName}' is full. Cannot move '{item.file_name}'.")
+            else:
+                print(f"Destination directory '{destination_path}' not found.")
+        else:
+            print(f"Item '{current_path}' not found.")
+
+    def get_item_from_directory(self, path):
+        path_parts = path.split('/')
+        item_name = path_parts.pop()
+        dir_path = '/'.join(path_parts)
+        directory = self.find_directory(dir_path)
+        if directory:
+            for item in directory.listOfFiles:
+                if item.file_name == item_name:
+                    return item
+            print(f"Item '{item_name}' not found in directory '{directory.dirName}'.")
+            return None
+        else:
+            print(f"Directory '{dir_path}' not found.")
+            return None
+
+    def get_items(self, current_dir=None):
+        if current_dir is None:
+            current_dir = self.root
+        items = []
+        for item in current_dir.listOfFiles:
+            if isinstance(item, Directory):
+                item_type = 'Directory'
+                item_name = item.dirName
+            elif isinstance(item, (BinaryFile, LogFile, BufferFile)):
+                item_type = 'File'
+                item_name = item.file_name
+            else:
+                continue
+
+            items.append({
+                'name': item_name,
+                'type': item_type
+            })
+
+        return json.dumps(items, indent=2)
+
+    def find_file(self, path):
+        path_parts = path.split('/')
+        current_dir = self.root
+        for part in path_parts:
+            if part == '':
+                continue
+            found = False
+            for item in current_dir.listOfFiles:
+                if isinstance(item, Directory) and item.dirName == part:
+                    current_dir = item
+                    found = True
+                    break
+                elif item.file_name == part:
+                    return item
+            if not found:
+                print(f"Path '{path}' not found.")
+                return None
+        print(f"Path '{path}' not found.")
+        return None
+
+    def delete_directory(self, path):
+        directory = self.find_directory(path)
+        if directory:
+            if directory.father is not None:
+                directory.father.listOfFiles.remove(directory)
+                directory.father.numberOfElements -= 1
+                directory.father = None
+                del directory
+                print(f"Directory '{path}' deleted.")
+            else:
+                print("Cannot delete root directory.")
+        else:
+            print(f"Directory '{path}' not found.")
+
+    def add_item(self, item, path):
+        target_dir = self.find_directory(path)
+        if target_dir:
+            if isinstance(item, Directory):
+                item_name = item.dirName
+            else:
+                item_name = item.file_name
+
+            if target_dir.numberOfElements < target_dir.DIR_MAX_ELEMS or target_dir.DIR_MAX_ELEMS == 0:
+                target_dir.listOfFiles.append(item)
+                target_dir.numberOfElements += 1
+                item.father = target_dir
+
+                print(f"Item '{item_name}' added to '{path}'.")
+            else:
+                print(f"Directory '{target_dir.dirName}' is full. Cannot add '{item_name}'.")
+        else:
+            print(f"Directory '{path}' not found.")
+
+
--- main.py
+++ main.py
@@ -1,22 +1,307 @@
-from fs import Directory, BinaryFile, LogFile, BufferFile
+from fs import Directory, BinaryFile, LogFile, BufferFile, FileSystem
+from flask import Flask, jsonify, request, redirect, url_for, abort
+import json
 
-print('this is lab1')
-root = Directory('root', 100)
-directory1 = Directory('child1', 20, root)
-directory2 = Directory('child2', 3, root)
-directory3 = Directory('chchild', 10, directory1)
+app = Flask(__name__)
 
-binaryFile1 = BinaryFile('binFile1', 'info1', root)
-binaryFile2 = BinaryFile('binFile2', 'info2', directory3)
-print(binaryFile2.read())
+root = Directory("root", 100)
+fs = FileSystem(root)
 
-logTextFile1 = LogFile('log1', 'loginfo1', root)
-logTextFile2 = LogFile('log2', 'This is', directory2)
-logTextFile2.append(' QA lab1')
-print(logTextFile2.read())
+fs.add_item(Directory('subroot1'), 'root')
+fs.add_item(Directory('subroot2'), 'root')
+b = BufferFile('buff')
+b.push('sdsd')
+b.push('dfgkjdfg')
+b.push('sdfsdfh')
+fs.add_item(BinaryFile('binFile', "dsdfksdbf"), 'root')
+fs.add_item(b, 'root')
 
-bufferFile1 = BufferFile('buf1', 5, root)
-bufferFile2 = BufferFile('buf2', 3, directory1)
-print(type(root.listOfFiles))
-print("-----------------------------------------------------------------------------------------")
-print(root.list_elements())
+
+@app.get("/directory/<path:directory_path>")
+def get_directory(directory_path):
+    directory = fs.find_directory(directory_path)
+
+    if directory is None:
+        return {'error_message': 'Directory not found'}, 400
+
+    return fs.get_items(directory)
+
+
+
+@app.get("/bufferfile/<path:file_path>")
+def get_buffer_file(file_path):
+    buffer_file = fs.find_file(file_path)
+
+    if buffer_file is None or type(buffer_file) is not BufferFile:
+        return {'error_message': 'File not found'}, 400
+
+    return fs.get_item(buffer_file)
+
+
+@app.get("/logfile/<path:file_path>")
+def get_log_file(file_path):
+    log_file = fs.find_file(file_path)
+
+    if log_file is None or type(log_file) is not LogFile:
+        return {'error_message': 'File not found'}, 400
+
+    return fs.get_item(log_file)
+
+
+@app.get("/binaryfile/<path:file_path>")
+def get_binary_file(file_path):
+    binary_file = fs.find_file(file_path)
+
+    if binary_file is None or type(binary_file) is not BinaryFile:
+        return {'error_message': 'File not found'}, 400
+
+    return fs.get_item(binary_file)
+
+
+@app.delete("/directory/<path:directory_path>")
+def delete_directory(directory_path):
+    directory = fs.find_directory(directory_path)
+
+    if directory is None or directory is not Directory:
+        return {'error_message': 'Directory not found'}, 400
+
+    is_removed = fs.delete_directory(directory)
+
+    if is_removed:
+        return {'success': True}
+    else:
+        return {'error_message': 'Directory could not be removed'}, 400
+
+
+@app.delete("/binaryfile/<path:file_path>")
+def delete_binaryfile(file_path):
+    bin_file = fs.find_file(file_path)
+
+    if bin_file is None or bin_file is not BinaryFile:
+        return {'error_message': 'BinaryFile not found'}, 400
+
+    fs.remove_item(bin_file)
+    return {'success': True}
+
+
+@app.delete("/logfile/<path:file_path>")
+def delete_logfile(file_path):
+    log_file = fs.find_file(file_path)
+
+    if log_file is None or log_file is not LogFile:
+        return {'error_message': 'LogFile not found'}, 400
+
+    fs.remove_item(log_file)
+    return {'success': True}
+
+
+@app.delete("/bufferfile/<path:file_path>")
+def delete_bufferfile(file_path):
+    buffer_file = fs.find_file(file_path)
+
+    if buffer_file is None or buffer_file is not BufferFile:
+        return {'error_message': 'LogFile not found'}, 400
+
+    fs.remove_item(buffer_file)
+    return {'success': True}
+
+
+@app.post("/directory/")
+def create_directory():
+    json = request.get_json()
+    directory_path = json.get('directory_path')
+    directory_name = json.get('directory_name')
+    new_directory = Directory(directory_name)
+    is_created = fs.add_item(new_directory, directory_path)
+
+    if is_created:
+        return {'success': True}
+    else:
+        return {'error_message': 'Item with such name already exists'}, 400
+
+
+@app.post("/bufferfile/")
+def create_bufferfile():
+    json = request.get_json()
+    directory_path = json.get('directory_path')
+    buffer_file_name = json.get('file_name')
+    new_file = BufferFile(buffer_file_name)
+    is_created = fs.add_item(new_file, directory_path)
+
+    if is_created:
+        return {'success': True}
+    else:
+        return {'error_message': 'Item with such name already exists'}, 400
+
+
+@app.post("/logfile/")
+def create_logfile():
+    json = request.get_json()
+    directory_path = json.get('directory_path')
+    binary_file_name = json.get('file_name')
+    new_file = LogFile(binary_file_name, "")
+    is_created = fs.add_item(new_file, directory_path)
+
+    if is_created:
+        return {'success': True}
+    else:
+        return {'error_message': 'Item with such name already exists'}, 400
+
+
+@app.post("/binaryfile/")
+def create_binaryfile():
+    json = request.get_json()
+    directory_path = json.get('directory_path')
+    binary_file_name = json.get('file_name')
+    file_content = json.get('content')
+    new_file = BinaryFile(binary_file_name, file_content)
+    is_created = fs.add_item(new_file, directory_path)
+
+    if is_created:
+        return {'success': True}
+    else:
+        return {'error_message': 'Item with such name already exists'}, 400
+
+
+@app.post("/logfile/log")
+def log_line_to_file():
+    json = request.get_json()
+    log_file_path = json.get('log_file_path')
+    log_line = json.get('log_line')
+
+    if log_line is None or log_file_path is None:
+        return {'error_message': 'Invalid data'}, 400
+
+    log_file = fs.find_file(log_file_path)
+
+    if log_file is None:
+        return {'error_message': 'File not found'}, 400
+
+    if type(log_file) is not LogFile:
+        return {'error_message': 'File do not supports logging'}, 400
+
+    log_file.append(log_line)
+    return {'success': True}
+
+
+@app.post("/bufferfile/push")
+def push_element_to_buffer_file():
+    json = request.get_json()
+    buffer_file_path = json.get('buffer_file_path')
+    element = json.get('push_element')
+
+    if buffer_file_path is None or element is None:
+        return {'error_message': 'Invalid data'}, 400
+
+    buffer_file = fs.find_file(buffer_file_path)
+
+    if buffer_file is None:
+        return {'error_message': 'File not found'}, 400
+
+    if type(buffer_file) is not BufferFile:
+        return {'error_message': 'This operation can not be done with this file'}, 400
+
+    is_pushed = buffer_file.push(element)
+
+    if is_pushed:
+        return {'success': True}
+    else:
+        return {'error_message': 'Element can not be pushed'}, 400
+
+
+@app.get("/bufferfile/pop/<path:buffer_file_path>")
+def pop_element_from_buffer_file(buffer_file_path):
+    buffer_file = fs.find_file(buffer_file_path)
+
+    if buffer_file is None:
+        return {'error_message': 'File not found'}, 400
+
+    if type(buffer_file) is not BufferFile:
+        return {'error_message': 'This operation can not be done with this file'}, 400
+
+    pop_element = buffer_file.pop()
+
+    if pop_element is not None:
+        return {'pop_element': pop_element}
+    else:
+        return {'error_message': 'Buffer file is empty'}, 400
+
+
+@app.post("/bufferfile/move")
+def move_bufferfile():
+    json = request.get_json()
+    buffer_file_path = json.get('buffer_file_path')
+    destination_path = json.get('destination_path')
+
+    buffer_file = fs.find_file(buffer_file_path)
+
+    if buffer_file is None or type(buffer_file) is not BufferFile:
+        return {'error_message': 'File not found'}, 400
+
+    is_moved = fs.move_item(buffer_file_path, destination_path)
+
+    if is_moved:
+        return {'success': True}
+    else:
+        return {'error_message': f'File can not be moved to {destination_path}'}, 400
+
+
+@app.post("/directory/move")
+def move_directory():
+    json = request.get_json()
+    directory_path = json.get('directory_path')
+    destination_path = json.get('destination_path')
+
+    directory = fs.find_directory(directory_path)
+
+    if directory is None or type(directory) is not Directory:
+        return {'error_message': 'File not found'}, 400
+
+    is_moved = fs.move_item(directory_path, destination_path)
+
+    if is_moved:
+        return {'success': True}
+    else:
+        return {'error_message': f'Directory can not be moved to {destination_path}'}, 400
+
+
+@app.post("/logfile/move")
+def move_logfile():
+    json = request.get_json()
+    log_file_path = json.get('log_file_path')
+    destination_path = json.get('destination_path')
+
+    log_file = fs.find_file(log_file_path)
+
+    if log_file is None or type(log_file) is not LogFile:
+        return {'error_message': 'File not found'}, 400
+
+    is_moved = fs.move_item(log_file_path, destination_path)
+
+    if is_moved:
+        return {'success': True}
+    else:
+        return {'error_message': f'File can not be moved to {destination_path}'}, 400
+
+
+@app.post("/binaryfile/move")
+def move_binaryfile():
+    json = request.get_json()
+    binary_file_path = json.get('binary_file_path')
+    destination_path = json.get('destination_path')
+
+    binary_file = fs.find_file(binary_file_path)
+
+    if binary_file is None or type(binary_file) is not BinaryFile:
+        return {'error_message': 'File not found'}, 400
+
+    is_moved = fs.move_item(binary_file_path, destination_path)
+
+    if is_moved:
+        return {'success': True}
+    else:
+        return {'error_message': f'File can not be moved to {destination_path}'}, 400
+
+
+if __name__ == '__main__':
+    app.run()
